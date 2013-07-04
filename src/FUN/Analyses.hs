87658ad6d39926b95620597efdf7610aee05c051
Wen Kokke <wenkokke@users.noreply.github.com>
2013-07-04 20:28:16 +0200

    inconsequential changes

--- src/FUN/Analyses.hs
+++ src/FUN/Analyses.hs
@@ -44,7 +44,8 @@ prelude = if False then return (mempty, []) else
 
 -- * Type definitions
 
-type TVar = Name -- Type Variable
+-- |Type variables
+type TVar = String
 
 data Type
   = TVar  TVar
@@ -100,6 +101,11 @@ ftv (TProd _ _ a b) = L.union (ftv a) (ftv b)
 ftv (TSum  _ _ a b) = L.union (ftv a) (ftv b)
 ftv (TUnit _ _)     = [ ]
 
+-- |Types of primitives.
+typeOf :: Lit -> Type
+typeOf (Bool _) = TBool
+typeOf (Integer s b _) = TInt s b
+
 -- |Representation for possible errors in algorithm W.
 data TypeError
   = CannotDestruct  Type      -- ^ thrown when attempting to destruct a non-product
@@ -125,10 +131,6 @@ instance Show TypeError where
   show (MeasureError     s) = "Unit of Measure Error: " ++ s
 
 type Analysis a = ErrorT TypeError (SupplyT FVar (Supply TVar)) a
-
-typeOf :: Lit -> Type
-typeOf (Bool    _) = TBool
-typeOf (Integer s b _) = TInt s b
 
 (~>) :: TVar -> Type -> Env -> Env
 x ~> t = \(m, w) -> (M.insert x t m, w)
