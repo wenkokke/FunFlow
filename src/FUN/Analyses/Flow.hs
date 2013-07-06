707a3544990acb97272b57f40e663e2a787feba7
wickedcodeninja <wicked@code.ninja>
2013-07-06 05:55:58 +0200

    Uniformity

--- src/FUN/Analyses/Flow.hs
+++ src/FUN/Analyses/Flow.hs
@@ -31,7 +31,7 @@ data Flow
 data FlowConstraint
   = Flow String -- ^ Type of the Program Point (Abs, Fix or some custom product/sum/unit)    
          Flow   -- ^ Annotation Variable associated to a Type
-         Label  -- ^ Program Point that reaches the associated with the Flow variable
+         Set Label  -- ^ Program Point that reaches the associated with the Flow variable
     deriving (Eq, Ord, Show)
     
     
@@ -39,8 +39,8 @@ data FlowConstraint
 --  obtain a mapping from Flow variables to sets of Program Points. Each flow variable is
 --  associated to a specific type that can occur multiple times in the program and each
 --  set constains program points that can reach this type.
-solveFlowConstraints :: Set FlowConstraint -> Map FVar (String, Set Label)
-solveFlowConstraints =
+solveFlowConstraints :: Set FlowConstraint -> Set FlowConstraint
+solveFlowConstraints = undefined {-
   let mergeNames p q = let (np, cp) = span (/= '.') p
                            (nq, cq) = span (/= '.') q
                        in if np == nq
@@ -50,7 +50,7 @@ solveFlowConstraints =
                              else error $ "different constructors used to construct sum type (\"" ++ np ++ "\" vs. \"" ++ nq ++ "\")"
       toFEnv (Flow nm (FVar r) l) = M.singleton r (nm, S.singleton l)
   in M.unionsWith (\(nx, vx) (ny, vy) -> (mergeNames nx ny, vx `union` vy) ) . S.toList . S.map toFEnv
-
+  -}
 -- |Pretty print the Annotated Type Variable -> Program Point Set map.
 --  Names between brackets correspond to Annotated Type Variables 
 --  and Names between brackets correspond to Program Points
